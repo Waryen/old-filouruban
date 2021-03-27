244135d7e3792e375f3d6ac85ae6958d2650438e
Waryen <jonathangomand@gmail.com>
2021-03-27 21:23:12 +0100

    Add: migration table messages

---
+++ website/database/migrations/2021_03_27_201906_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content', 1000);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->unsignedBigInteger('admins_id');
+            $table->foreign('admins_id')->references('id')->on('admins')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
