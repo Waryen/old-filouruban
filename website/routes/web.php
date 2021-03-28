bc546d529c6415526f166b62e21d9905b8066f5f
Waryen <jonathangomand@gmail.com>
2021-03-28 16:55:52 +0200

    Add: Routes Controllers public publics + pages view de test

--- website/routes/web.php
+++ website/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// Controllers
+// Controllers Admin
 use App\Http\Controllers\AdminsController;
 use App\Http\Controllers\ArticlesController;
 use App\Http\Controllers\CategoriesController;
@@ -9,6 +9,14 @@ use App\Http\Controllers\CommentariesController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\MessagesController;
 use App\Http\Controllers\SubscribersController;
+// Controllers Public
+use App\Http\Controllers\public\AdminsPublicController;
+use App\Http\Controllers\public\ArticlesPublicController;
+use App\Http\Controllers\public\CategoriesPublicController;
+use App\Http\Controllers\public\CommentairesPublicController;
+use App\Http\Controllers\public\ContactPublicController;
+use App\Http\Controllers\public\MessagesPublicController;
+use App\Http\Controllers\public\SubscribersPublicController;
 
 /*
 |--------------------------------------------------------------------------
