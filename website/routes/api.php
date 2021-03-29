5b30c801386017abfab308064ed732ddd8c1c2d5
Waryen <jonathangomand@gmail.com>
2021-03-29 18:59:56 +0200

    Edit: Route API et authentification

--- website/routes/api.php
+++ website/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AdminsController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Admins
+Route::middleware(['auth:api'])->group(function () {
+    Route::apiResource('admins', AdminsController::class);
 });
