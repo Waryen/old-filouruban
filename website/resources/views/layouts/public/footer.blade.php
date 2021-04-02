3af29580da9a76d64695e5e395f4708f270302ed
Waryen <jonathangomand@gmail.com>
2021-04-02 19:56:15 +0200

    Edit: header & footer responsive

--- website/resources/views/layouts/public/footer.blade.php
+++ website/resources/views/layouts/public/footer.blade.php
@@ -1,30 +1,24 @@
 <footer class="footer">
-    <div class="footer-left">
+    <nav class="footer-nav">
+        <ul>
+            <li><a href="#">A propos de Fil ou ruban</a></li>
+            <li><a href="#">Mentions légales</a></li>
+        </ul>
+    </nav>
+    <div class="footer-newsletter" id="react-newsletter"></div>
+    <div class="copyrights">
+        <p><strong>Copyrights © 2021 | filouruban.be</strong></p>
+    </div>
+    <div class="social-media">
+        <p>Suivez-moi</p>
         <nav>
             <ul>
-                <li><a href="#">A propos de Fil ou ruban</a></li>
-                <li><a href="#">Mentions légales</a></li>
+                <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
             </ul>
         </nav>
     </div>
-    <div class="footer-center">
-        <div class="footer-newsletter" id="react-newsletter"></div>
-        <div class="copyrights">
-            <p><strong>© 2021 | filouruban.be</strong></p>
-        </div>
-    </div>
-    <div class="footer-right">
-        <div class="social-media">
-            <p>Suivez-moi</p>
-            <nav>
-                <ul>
-                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                </ul>
-            </nav>
-        </div>
-        <div class="developer-credits">
-            <p>Made with love by Jonathan Gomand</p>
-        </div>
+    <div class="developer-credits">
+        <p>Made with love by Jonathan Gomand</p>
     </div>
 </footer>
