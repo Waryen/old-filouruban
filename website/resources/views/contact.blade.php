2fd4712fafb82934ef85afab806819aaaae2322b
Waryen <jonathangomand@gmail.com>
2021-05-06 18:28:00 +0200

    Edit: page contact CSS

--- website/resources/views/contact.blade.php
+++ website/resources/views/contact.blade.php
@@ -8,5 +8,18 @@
     <div class="contact">
         <h2 class="contact-title">Contacter Filouruban</h2>
         <div id="react-contact"></div>
+        <div class="social-media">
+            <h3 class="social-title">Vous pouvez également nous contacter via nos réseaux sociaux</h3>
+            <ul>
+                <li>
+                    <h4>Facebook</h4>
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                </li>
+                <li>
+                    <h4>Instagram</h4>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+            </ul>
+        </div>
     </div>
 @endsection
