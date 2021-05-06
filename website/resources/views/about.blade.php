7cdadc23f82fbf879394eacd2d72fcdda495af56
Waryen <jonathangomand@gmail.com>
2021-05-06 17:17:40 +0200

    Add: page about CSS

--- website/resources/views/about.blade.php
+++ website/resources/views/about.blade.php
@@ -5,10 +5,12 @@
 @endsection
 
 @section('body')
-    <h2 class="main-title">A propos de Filouruban</h2>
-    <div class="about-content">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad nam eum, ipsum temporibus voluptatum dolorem similique, aliquid laudantium, soluta neque voluptatem velit! Id adipisci fugiat, dolore aspernatur iusto blanditiis!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non numquam veritatis autem doloribus aliquam odit aut eius aperiam. Eaque quis vel ipsa! Odio ut iste quaerat, excepturi recusandae dolorum atque.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse deserunt labore quia blanditiis excepturi placeat necessitatibus porro accusamus consequatur eaque ab, libero minus amet, ratione omnis tempora ipsa ea. Accusamus!</p>
+    <div class="about">
+        <h2 class="about-title">A propos de Filouruban</h2>
+        <div class="about-content">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad nam eum, ipsum temporibus voluptatum dolorem similique, aliquid laudantium, soluta neque voluptatem velit! Id adipisci fugiat, dolore aspernatur iusto blanditiis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non numquam veritatis autem doloribus aliquam odit aut eius aperiam. Eaque quis vel ipsa! Odio ut iste quaerat, excepturi recusandae dolorum atque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse deserunt labore quia blanditiis excepturi placeat necessitatibus porro accusamus consequatur eaque ab, libero minus amet, ratione omnis tempora ipsa ea. Accusamus!</p>
+        </div>
     </div>
 @endsection
