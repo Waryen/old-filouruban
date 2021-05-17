b0fe74e79a74fc24ca71cb23bda8bc340105e190
Waryen <jonathangomand@gmail.com>
2021-05-17 17:21:06 +0200

    Add: envoi à Sendinblue lorsqu'un article est créé ou qu'un abonné est supprimé

--- website/app/Http/Controllers/ArticleController.php
+++ website/app/Http/Controllers/ArticleController.php
@@ -26,7 +26,35 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        Article::create($request->all());
+        // Demande à Sendinblue d'informer les abonnés de l'ajout d'un nouvel article
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+        CURLOPT_URL => "https://api.sendinblue.com/v3/emailCampaigns/2/sendNow",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_HTTPHEADER => [
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "api-key: ".env('SENDINBLUE_API_KEY')
+        ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+            Article::create($request->all());
+        }
     }
 
     /**
--- website/app/Http/Controllers/SubscriberController.php
+++ website/app/Http/Controllers/SubscriberController.php
@@ -127,6 +127,34 @@ class SubscriberController extends Controller
      */
     public function destroy(Subscriber $subscriber)
     {
-        $subscriber->delete();
+        // Supprime également l'abonné sur Sendinblue
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+        CURLOPT_URL => "https://api.sendinblue.com/v3/contacts/".$subscriber->email,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "DELETE",
+        CURLOPT_HTTPHEADER => [
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "api-key: ".env('SENDINBLUE_API_KEY')
+        ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+            $subscriber->delete();
+        }
     }
 }
